c73ddc8367506ef188be47119b545c6dc6b86f9d
laizq <123632704@qq.com>
2022-03-07 02:44:05 +0800

    all.css'problem

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,14 +12,13 @@ export default {
             { name: 'format-detection', content: 'telephone=no' },
         ],
         link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-            { rel: 'stylesheet', type: 'text/css', href: '/all.css' }
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
         ],
     },
 
     // Global CSS: https://go.nuxtjs.dev/config-css
     css: [
-        'swiper/dist/css/swiper.min.css'
+        'swiper/dist/css/swiper.min.css', '~/style/all.css'
     ],
 
     // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
