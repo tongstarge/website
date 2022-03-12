18c7903fdad3ec8364661ed38e65814545f197cf
laizq <123632704@qq.com>
2022-03-12 13:45:26 +0800

    footer

---
+++ components/footer.vue
@@ -0,0 +1,168 @@
+<template>
+  <div class="footer">
+    <div class="footer-box">
+      <div class="footer-item" v-for="(f, key) in footer_items" :key="key">
+        <div class="footer-item-h">{{ f.headline }}</div>
+        <div class="footer-item-p" v-for="(fp, k) in f.items" :key="k">
+          <a :href="fp.url" target="_blank" rel="noopener noreferrer">
+            {{ fp.text }}
+          </a>
+        </div>
+      </div>
+      <div class="footer-item">
+        <div class="footer-other">
+          <div class="footer-other-item">
+            <a
+              href="https://space.bilibili.com/444138308?from=search&seid=3399642198415679525"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                class="footer-other-item-icon"
+                :src="require('../assets/bilibili.svg')"
+              />
+            </a>
+          </div>
+          <div class="footer-other-item">
+            <a
+              href="https://qm.qq.com/cgi-bin/qm/qr?k=nT-42kovrX3KEPoiwo8DlERw27LGmBUu&jump_from=webapi"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                style="width: 30px; margin-top: 35px; height: 30px"
+                class="footer-other-item-icon"
+                :src="require('../assets/qq.svg')"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer-bottom">© 2022. All Rights Reserved.曈星阁 版权所有</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      footer_items: [
+        {
+          headline: '加入我们',
+          items: [
+            {
+              text: 'qq频道',
+              url: 'https://qun.qq.com/qqweb/qunpro/share?_wv=3&_wwv=128&inviteCode=zyeHi&from=246610&biz=ka',
+            },
+            {
+              text: '成为内部成员',
+              url: 'https://wj.qq.com/s2/8706484/a8ff',
+            },
+          ],
+        },
+        {
+          headline: '传送门',
+          items: [
+            {
+              text: '曈星阁家园',
+              url: 'https://www.tongstarge.com',
+            },
+            /** {
+              text: '源界',
+              url: 'https://yc.tongstarge.cn',
+            },
+            {
+              text: '自学直达',
+              url: 'https://learn.tongstarge.cn',
+            },**/
+          ],
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style>
+.footer {
+  width: 100vw;
+  text-align: center;
+}
+.footer-box {
+  padding: 5vw 20vh;
+}
+.footer-item {
+  display: inline-block;
+  vertical-align: top;
+  padding: 50px 40px;
+  text-align: left;
+}
+@media (max-width: 700px) {
+  .footer-item {
+    display: block !important;
+    text-align: center !important;
+    padding: 0 !important;
+  }
+  .footer-box {
+    padding: 5vh 2vw;
+  }
+  .footer-other-item {
+    width: 80vw !important;
+    display: block !important;
+  }
+}
+.footer-item-h {
+  color: #50616d;
+  font-family: '思源宋体';
+  font-weight: 800;
+  margin-bottom: 15px;
+  font-size: 17px;
+}
+.footer-item-p {
+  color: #50616d99;
+  transition: 0.2s;
+  font-size: 15px;
+  line-height: 30px;
+  cursor: pointer;
+}
+.footer-item-p a:hover {
+  color: #ff5100;
+}
+.footer-item-p a {
+  text-decoration: none;
+  color: inherit;
+}
+.footer-other {
+  border-left: 1px solid #50616dcc;
+  padding-left: 20px;
+}
+.footer-other-item-icon {
+  width: 50px;
+  height: 50px;
+  line-height: 100px;
+  display: inline-block;
+  vertical-align: top;
+  color: #fff !important;
+}
+.footer-other-item {
+  color: #bbb;
+  width: 100px;
+  text-align: center;
+  height: 100px;
+  display: inline-block;
+  vertical-align: top;
+}
+.footer-other-item-icon {
+  width: 40px;
+  margin-top: 30px;
+  height: 40px;
+}
+.footer-bottom {
+  text-align: center;
+  font-size: 14px;
+  padding: 10px;
+  color: #ffffffcc;
+  background-color: #3d3b4f;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,71 +1,80 @@
 <template>
   <div class="home">
-    <Navbar />
-    <div class="topbox">
-      <div style="text-align: center">
-        <div class="headline" style="color: #ffffff; padding-bottom: 30px">
-          曈星阁
+    <no-ssr>
+      <Navbar :style="'margin-top:'" />
+      <div class="topbox">
+        <div style="text-align: center">
+          <div class="headline" style="color: #ffffff; padding-bottom: 30px">
+            曈星阁
+          </div>
+          <div class="text">一群少年向着梦想前进</div>
         </div>
-        <div class="text">一群少年向着梦想前进</div>
+        <div class="s"></div>
       </div>
-      <div class="s"></div>
-    </div>
-    <div class="box">
-      <div>
-        <div class="headline">管理模式</div>
-        <div class="home-mode">
-          <div class="home-mode-item" v-for="(m, key) in mode_items" :key="key">
-            <div class="home-mode-item-img_box">
-              <div
-                class="home-mode-item-img"
-                :style="
-                  'background-image:url(https://p.qlogo.cn/gh/' +
-                  m.group_n +
-                  '/' +
-                  m.group_n +
-                  '/640)'
-                "
-              ></div>
-            </div>
-            <div class="home-mode-item-content">
-              <div class="home-mode-item-name">
-                {{ m.name }}
+      <div class="box">
+        <div>
+          <div class="headline">管理模式</div>
+          <div class="home-mode">
+            <div
+              class="home-mode-item"
+              v-for="(m, key) in mode_items"
+              :key="key"
+            >
+              <div class="home-mode-item-img_box">
+                <div
+                  class="home-mode-item-img"
+                  :style="
+                    'background-image:url(https://p.qlogo.cn/gh/' +
+                    m.group_n +
+                    '/' +
+                    m.group_n +
+                    '/640)'
+                  "
+                ></div>
               </div>
-              <div class="home-mode-item-introduce">
-                {{ m.introduce }}
+              <div class="home-mode-item-content">
+                <div class="home-mode-item-name">
+                  {{ m.name }}
+                </div>
+                <div class="home-mode-item-introduce">
+                  {{ m.introduce }}
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    <div class="box" style="background: #f2ecde">
-      <div>
-        <div class="headline">遇见有趣的人</div>
-        <div v-swiper:mySwiper_member="swiperOption_member" class="home-member">
-          <div class="swiper-wrapper">
-            <div class="swiper-slide" v-for="(m, key) in members" :key="key">
-              <div class="home-member-item">
-                <div
-                  class="home-member-item-avatar"
-                  :style="
-                    'background-image:url(http://q1.qlogo.cn/g?b=qq&nk=' +
-                    m.qq_number +
-                    '&s=640)'
-                  "
-                ></div>
-                <div class="home-member-item-name">
-                  {{ m.name }}
-                </div>
-                <div class="home-member-item-info">
-                  {{ m.info }}
-                </div>
-                <div class="home-member-item-footer">
+      <div class="box" style="background: #f2ecde">
+        <div>
+          <div class="headline">遇见有趣的人</div>
+          <div
+            v-swiper:mySwiper_member="swiperOption_member"
+            class="home-member"
+          >
+            <div class="swiper-wrapper">
+              <div class="swiper-slide" v-for="(m, key) in members" :key="key">
+                <div class="home-member-item">
                   <div
-                    class="home-member-item-footer-btn"
-                    @click="getinfo(key)"
-                  >
-                    取得联系
+                    class="home-member-item-avatar"
+                    :style="
+                      'background-image:url(http://q1.qlogo.cn/g?b=qq&nk=' +
+                      m.qq_number +
+                      '&s=640)'
+                    "
+                  ></div>
+                  <div class="home-member-item-name">
+                    {{ m.name }}
+                  </div>
+                  <div class="home-member-item-info">
+                    {{ m.info }}
+                  </div>
+                  <div class="home-member-item-footer">
+                    <div
+                      class="home-member-item-footer-btn"
+                      @click="getinfo(key)"
+                    >
+                      取得联系
+                    </div>
                   </div>
                 </div>
               </div>
@@ -73,47 +82,42 @@
           </div>
         </div>
       </div>
-    </div>
-    <s-dialog v-model="m_dialog" animation="zoom"> 12121 </s-dialog>
-    <div class="box">
-      <div>
-        <div class="headline">最近在干什么</div>
-        <div v-swiper:mySwiper_notice="swiperOption_notice" class="home-notice">
-          <div class="swiper-wrapper">
-            <div class="swiper-slide" v-for="(n, key) in notices" :key="key">
-              <div class="home-notice-item">
-                <div class="home-notice-item-title">
-                  {{ n.title }}
-                </div>
-                <div class="home-notice-item-content">
-                  {{ n.content }}
+      <s-dialog v-model="m_dialog" animation="zoom"> 12121 </s-dialog>
+      <div class="box">
+        <div>
+          <div class="headline">最近在干什么</div>
+          <div
+            v-swiper:mySwiper_notice="swiperOption_notice"
+            class="home-notice"
+          >
+            <div class="swiper-wrapper">
+              <div class="swiper-slide" v-for="(n, key) in notices" :key="key">
+                <div class="home-notice-item">
+                  <div class="home-notice-item-title">
+                    {{ n.title }}
+                  </div>
+                  <div class="home-notice-item-content">
+                    {{ n.content }}
+                  </div>
                 </div>
               </div>
             </div>
+            <div class="swiper-pagination"></div>
           </div>
-          <div class="swiper-pagination"></div>
         </div>
       </div>
-    </div>
-    <div class="home-join">
-      <div class="home-join-bg" :class="bg ? 'home-join-bg_a' : ''"></div>
-      <div
-        class="home-join-btn"
-        @mouseenter="bg = true"
-        @mouseleave="bg = false"
-      >
-        加入我们
+      <div class="home-join">
+        <div class="home-join-bg" :class="bg ? 'home-join-bg_a' : ''"></div>
+        <div
+          class="home-join-btn"
+          @mouseenter="bg = true"
+          @mouseleave="bg = false"
+        >
+          加入我们
+        </div>
       </div>
-    </div>
-    <div class="footer">
-      <div class="footer-content">
-        <div class="footer-left"></div>
-        <div class="footer-right">.footer</div>
-      </div>
-      <div class="footer-bottom">
-        © 2022. All Rights Reserved.曈星阁 版权所有
-      </div>
-    </div>
+      <Footer />
+    </no-ssr>
   </div>
 </template>
 <style>
@@ -131,11 +135,13 @@
 <script lang="ts">
 import Vue from 'vue'
 import Navbar from '../components/navbar.vue'
+import Footer from '../components/footer.vue'
 
 export default Vue.extend({
   name: 'IndexPage',
   components: {
     Navbar,
+    Footer,
   },
   data() {
     return {
@@ -147,7 +153,6 @@ export default Vue.extend({
       },
       bg: false,
       swiperOption_notice: {
-        effect: 'cards',
         loop: false,
         autoplay: false,
         slidesPerView: 1,
