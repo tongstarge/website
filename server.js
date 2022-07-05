882fba9d360c7a5d2548c7a569f89577bdf6351f
laizq <123632704@qq.com>
2022-07-05 19:34:38 +0800

    尝试使用@fastify/static

--- server.js
+++ server.js
@@ -1,29 +1,30 @@
 const fastify = require('fastify')({
     logger: true
 })
-const { readFileSync } = require('fs');
+const fastifyStatic = require('@fastify/static')
 const path = require('path');
 
-fastify.get('/', function (request, reply) {
-    const file = path.join(process.cwd(), 'app', 'index.html');
-    const web = readFileSync(file, 'utf-8')
-    reply.type('text/html').send(web)
+fastify.register(fastifyStatic, {
+    root: path.join(__dirname, 'app'),
+    serve: false 
 })
-/*fastify.get('/image/*', function (request, reply) {
-    const file = path.join(process.cwd(), 'app', request.url);
-    const files = readFileSync(file)
-    reply.type('image/x-icon').send(files)
-})*/
-fastify.get('*', function (request, reply) {
-    const file = path.join(process.cwd(), 'app', '404.html');
-    const web = readFileSync(file, 'utf-8')
-    reply.type('text/html').send(web)
+fastify.register(fastifyStatic, {
+    root: path.join(__dirname, 'app/static'),
+    prefix: '/static/',
+    decorateReply: false
 })
 
-fastify.listen(3000, function (err, address) {
+fastify.get('/', function (request, reply) {
+    reply.sendFile('index.html');
+})
+
+fastify.get('*', function (request, reply) {
+    reply.sendFile('404.html');
+})
+
+fastify.listen({ port: 3000 }, function (err, address) {
     if (err) {
         fastify.log.error(err)
         process.exit(1)
     }
-    fastify.log.info(`server listening on ${address}`)
 })
